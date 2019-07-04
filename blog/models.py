9b5f9eb1bba2f802f2869da2cccb1a70b9868b61
tnj0324 <mtg.life0324@gmail.com>
2019-07-04 19:05:39 +0900

    djangoG

--- blog/models.py
+++ blog/models.py
@@ -12,11 +12,13 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+
     def __str__(self):
         return self.title
 
     def approved_comments(self):
         return self.comments.filter(approved_comment=True)
+
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
@@ -25,9 +27,11 @@ class Comment(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
 
+
     def approve(self):
-        self.approve_comment = True
+        self.approved_comment = True
         self.save()
+
 
     def __str__(self):
         return self.text
--- blog/views.py
+++ blog/views.py
@@ -83,5 +83,6 @@ def comment_approve(request, pk):
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
-    return redirect('post_detail, pk=comment.post.pk')        
+    return redirect('post_detail', pk=comment.post.pk)
+        
 #Create your views here.
